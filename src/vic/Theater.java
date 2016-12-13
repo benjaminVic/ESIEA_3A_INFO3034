5942987c08925676ffd06b19d68860708e38ad08
benjaminVic <benjamin.vic.lallart@gmail.com>
2016-12-13 14:36:24 +0100

    WIP writing things
    adding content, left todo the actual content writting

--- src/vic/Theater.java
+++ src/vic/Theater.java
@@ -2,79 +2,126 @@ package vic;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Theater {
 
 	private Seat[][] seats;
 	private String filename;
-	
-	public Theater (int nbRow, int nbCol){
+	private final String sourceFilePath;
+
+	public Theater(int nbRow, int nbCol) {
 		this.seats = new Seat[nbRow][nbCol];
-		for (int rowIterator = 0 ; rowIterator<nbRow ; rowIterator++){
-			for (int colIterator = 0 ; colIterator<nbCol ; colIterator++){
-				this.seats[rowIterator][colIterator] = new Seat(rowIterator, colIterator, SeatType.FIRST_CATEGORY, false);
+		for (int rowIterator = 0; rowIterator < nbRow; rowIterator++) {
+			for (int colIterator = 0; colIterator < nbCol; colIterator++) {
+				this.seats[rowIterator][colIterator] = new Seat(rowIterator, colIterator, SeatType.FIRST_CATEGORY,
+						false);
+			}
+		}
+		this.sourceFilePath = null;
+	}
+
+	public Theater(String filename) throws FileNotFoundException {
+		this.sourceFilePath = filename;
+		File f = new File(filename);
+		Scanner sc = new Scanner(f, "UTF-8");
+		sc.useDelimiter(sc.delimiter() + "|;+");
+
+		// On récupère les premier params
+		int rowLenght = Integer.valueOf(sc.next());
+		int columnLenght = Integer.valueOf(sc.next());
+
+		this.seats = new Seat[rowLenght][columnLenght];
+		
+		for (int row = 0; row < rowLenght; row++) {
+			for (int col = 0 ; col < columnLenght; col++) {
+				String valeurLue = sc.next();
+				if (!Objects.equals(valeurLue,"")) {
+					seats[row][col] = new Seat(row, col, 
+							SeatType.getSeatTypeFromSymbole(valeurLue),Character.isUpperCase(valeurLue.charAt(0)));
+				} else {
+					col--;
+				}
+				// seats[row][col] = new Seat(row, col, SeatType.getSeatTypeFromSymbole(symbole),Character.isUpperCase(symbole.charAt(0)));
 			}
 		}
 	}
-	
-	public Theater (String filename) throws FileNotFoundException{
-		File f = new File(filename);
-		Scanner sc = new Scanner(f, "UTF-8");
-		sc.useDelimiter(sc.delimiter()+"|;+");
-		while(sc.hasNext()){
-			System.out.println(sc.next());
-			//seats[row][col] = new Seat(row, col, SeatType.getSeatTypeFromSymbole(symbole),Character.isUpperCase(symbole.charAt(0)));
-		}
-	}
-	
-	public int getNbRow(){
+
+	public int getNbRow() {
 		return seats.length;
 	}
-	
-	public int getNbCol(){
+
+	public int getNbCol() {
 		return seats[0].length;
 	}
-	
-	public String toString(){
+
+	public String toString() {
 		StringBuffer sb = new StringBuffer();
-		for (int rowIterator = 0 ; rowIterator < getNbRow()  ; rowIterator++) {
-			sb.append((char)('A'+rowIterator)+" ");
-			for (int columnIterator = 0 ; columnIterator<getNbCol() ; columnIterator++){
-				//En caps si le siège est réservé
-				if (seats[rowIterator][columnIterator].isBooked()){
-					sb.append('A');
+		for (int rowIterator = 0; rowIterator < getNbRow(); rowIterator++) {
+			sb.append((char) ('A' + rowIterator) + " ");
+			for (int columnIterator = 0; columnIterator < getNbCol(); columnIterator++) {
+				// En caps si le siège est réservé
+				if (seats[rowIterator][columnIterator].isBooked()) {
+					sb.append(seats[rowIterator][columnIterator].getType().getSymbole().toUpperCase());
 				} else {
-					sb.append('a');
+					sb.append(seats[rowIterator][columnIterator].getType().getSymbole().toLowerCase());
 				}
 				sb.append(' ');
 			}
 			sb.append('\n');
 		}
-		
-		//On affiche la ligne de numéro de colonne
+
+		// On affiche la ligne de numéro de colonne
 		sb.append(" ");
-		for (int columnIterator = 0 ; columnIterator < getNbCol() ; columnIterator++){
+		for (int columnIterator = 0; columnIterator < getNbCol(); columnIterator++) {
 			sb.append(" " + columnIterator);
 		}
 		sb.append('\n');
-		
+
 		return sb.toString();
 	}
-	
-	public void makeReservation(int row, int col) throws InvalidActionException{
-		if (seats[row][col].isBooked()){
+
+	public void makeReservation(int row, int col) throws InvalidActionException {
+		if (seats[row][col].isBooked()) {
 			throw new InvalidActionException("Il y a déjà une réservation à cet emplacement.");
 		} else {
 			seats[row][col].setBooked(true);
 		}
 	}
-	
-	public void cancelReservation(int row, int col)	throws InvalidActionException{
-		if (seats[row][col].isBooked()){
+
+	public void cancelReservation(int row, int col) throws InvalidActionException {
+		if (seats[row][col].isBooked()) {
 			seats[row][col].setBooked(false);
 		} else {
 			throw new InvalidActionException("Il n'y au aucune reservation à cet emplacement.");
+		}
+	}
+
+	public void save(){
+		//Setting filename
+		String newFileNamePath;
+		if (Objects.isNull(sourceFilePath)){
+			newFileNamePath = "./theatre.bk;";
+		} else {
+			newFileNamePath = sourceFilePath+".bk";
+		}
+		
+		//Actually saving shit
+		try {
+			FileWriter fw = new FileWriter(newFileNamePath);
+			StringBuilder sb = new StringBuilder();
+			sb.append(getNbRow() + ";" + getNbCol()+"\n");
+			for (int rowIterator = 0; rowIterator < getNbRow(); rowIterator++) {
+				for (int colIterator = 0; colIterator < getNbCol(); colIterator++) {
+					//TODO write the table
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 	
